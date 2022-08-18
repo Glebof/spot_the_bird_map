4261350937197548935091c4b1bdb7b4ee7ebde8
vip <20frag08@mail.ru>
2022-08-18 16:54:43 +0300

    refactor, create table db

--- lib/bloc/bird_post_cubit.dart
+++ lib/bloc/bird_post_cubit.dart
@@ -1,7 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import '../models/bird_post_model.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
 import 'dart:io';
 
@@ -14,31 +14,31 @@ class BirdPostCubit extends Cubit<BirdPostState> {
   Future<void> loadPosts() async {
     emit(state.copyWith(status: BirdPostStatus.loading));
 
-    SharedPreferences prefs = await SharedPreferences.getInstance();
+    // SharedPreferences prefs = await SharedPreferences.getInstance();
 
     List<BirdModel> birdPosts = [];
-
-    final List<String>? birdPostsJsonStringList =
-        prefs.getStringList("birdPosts");
-    if (birdPostsJsonStringList != null) {
-      for (var postJsonData in birdPostsJsonStringList) {
-        final Map<String, dynamic> data =
-            await json.decode(postJsonData) as Map<String, dynamic>;
-
-        final File imageFile = File(data["filePath"] as String);
-        final String birdName = data["birdName"] as String;
-        final String birdDescription = data["birdDescription"] as String;
-        final double latitude = data["latitude"] as double;
-        final double longitude = data["longitude"] as double;
-
-        birdPosts.add(BirdModel(
-            image: imageFile,
-            longitude: longitude,
-            latitude: latitude,
-            birdDescription: birdDescription,
-            birdName: birdName));
-      }
-    }
+    //
+    // final List<String>? birdPostsJsonStringList =
+    //     prefs.getStringList("birdPosts");
+    // if (birdPostsJsonStringList != null) {
+    //   for (var postJsonData in birdPostsJsonStringList) {
+    //     final Map<String, dynamic> data =
+    //         await json.decode(postJsonData) as Map<String, dynamic>;
+    //
+    //     final File imageFile = File(data["filePath"] as String);
+    //     final String birdName = data["birdName"] as String;
+    //     final String birdDescription = data["birdDescription"] as String;
+    //     final double latitude = data["latitude"] as double;
+    //     final double longitude = data["longitude"] as double;
+    //
+    //     birdPosts.add(BirdModel(
+    //         image: imageFile,
+    //         longitude: longitude,
+    //         latitude: latitude,
+    //         birdDescription: birdDescription,
+    //         birdName: birdName));
+    //   }
+    // }
     emit(state.copyWith(birdPosts: birdPosts, status: BirdPostStatus.loaded));
   }
 
@@ -49,7 +49,7 @@ class BirdPostCubit extends Cubit<BirdPostState> {
 
     birdPosts.add(birdModel);
 
-    _saveToSharedPrefs(birdPosts);
+    // _saveToSharedPrefs(birdPosts);
     emit(state.copyWith(birdPosts: birdPosts, status: BirdPostStatus.loaded));
   }
 
@@ -60,24 +60,24 @@ class BirdPostCubit extends Cubit<BirdPostState> {
 
     birdPosts.removeWhere((element) => element == birdModel);
 
-    _saveToSharedPrefs(birdPosts);
+    // _saveToSharedPrefs(birdPosts);
 
     emit(state.copyWith(birdPosts: birdPosts, status: BirdPostStatus.loaded));
   }
 
-  Future<void> _saveToSharedPrefs(List<BirdModel> posts) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-
-    final List<String> jsonDataList = posts
-        .map((post) => json.encode({
-              "birdName": post.birdName,
-              "birdDescription": post.birdDescription,
-              "latitude": post.latitude,
-              "longitude": post.longitude,
-              "filePath": post.image.path,
-            }))
-        .toList();
-
-    prefs.setStringList("birdPosts", jsonDataList);
-  }
+// Future<void> _saveToSharedPrefs(List<BirdModel> posts) async {
+//   // SharedPreferences prefs = await SharedPreferences.getInstance();
+//
+//   final List<String> jsonDataList = posts
+//       .map((post) => json.encode({
+//             "birdName": post.birdName,
+//             "birdDescription": post.birdDescription,
+//             "latitude": post.latitude,
+//             "longitude": post.longitude,
+//             "filePath": post.image.path,
+//           }))
+//       .toList();
+//
+//   prefs.setStringList("birdPosts", jsonDataList);
+// }
 }
